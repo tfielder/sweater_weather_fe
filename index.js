b867feac494106cc1aeaf4cc534abf983dc3a277
Tim Fielder <fiel1139@gmail.com>
2018-11-27 18:33:30 -0700

    Changes cursor to a pointer on buttons.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // This file is in the entry point in your webpack config.
 $(document).ready(function () {
 
-  $(".js-login-button").click(function () {
-    $(".login-modal").show();
+  $("button").each(function () {
+    $(this).css("cursor", "pointer");
   });
 
+  $(".submit").each(function () {
+    $(this).css("cursor", "pointer");
+  });
+
+  $(".js-login-button").click(function () {
+    $(".login-modal").show();
+
+  });
 
 });
