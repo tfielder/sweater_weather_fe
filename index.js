1f7276495a78300c2606f6c920d4db49505e3382
Tim Fielder <fiel1139@gmail.com>
2018-11-27 17:30:18 -0700

    Creates functioning JS for Login Button and Modal appearance. Adds Styling to login modal.

---
+++ index.js
@@ -0,0 +1,9 @@
+// This file is in the entry point in your webpack config.
+$(document).ready(function () {
+
+  $(".js-login-button").click(function () {
+    $(".login-modal").show();
+  });
+
+
+});
