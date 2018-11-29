3d007e1f9308f91776bb4bc2fb3ee3468b50453e
Tim Fielder <fiel1139@gmail.com>
2018-11-28 22:10:58 -0700

    Adds fetch request to location search button js.

--- index.js
+++ index.js
@@ -20,3 +20,16 @@
     $(".location-modal").show();
   });
 
+  $(".location-search").click(function (){
+    var location = $("#location").val();
+    const url = "https://whether-to-weather.herokuapp.com/api/v1/backgrounds?location=" + location;
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(locationPicture) {
+        JSON.stringify(locationPicture);
+        //debugger;
+      });
+  });
+
